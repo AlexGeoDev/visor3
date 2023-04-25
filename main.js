d1251e9a1014021e2dbb755cedde45991e9e7ced
alexgeodev <jaabec@gmail.com>
2023-04-25 09:42:54 -0500

    Branch visor1 created and added to visores

--- main.js
+++ main.js
@@ -1,64 +1,24 @@
-import './style.css'
-import Map from 'ol/Map.js';
-import View from 'ol/View.js';
-import { Stroke, Style} from 'ol/style.js';
-import {Draw, Modify} from 'ol/interaction.js';
-import {OSM, Vector as VectorSource} from 'ol/source.js';
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import './style.css';
+import  'bootstrap/dist/css/bootstrap.css'
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import Stamen from 'ol/source/Stamen';
 
-const typeSelect = document.getElementById("type");
-
-//Set the color and width of line go to draw
-const style = new Style({
-  stroke: new Stroke({
-    color: 'rgb(0, 0, 0)',
-    line: 1,
-    width: 3,
-  })
-});
-
-const raster = new TileLayer({
-  source: new OSM(),
-});
-
-const source = new VectorSource();
-
-function styleFunction() {
-  const styles = [style];
-  return styles;
+function baseMap() {
+  const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new Stamen({
+          layer: 'watercolor',
+        }),
+      }),
+    ],
+    view: new View({
+      center: [-7400000, 400000],
+      zoom: 4
+    })
+  });
 }
 
-//keep the style of line 
-const vector = new VectorLayer({
-  source: source,
-  style: function (feature) {
-    return styleFunction(feature);
-  },
-});
-
-const map = new Map({
-  layers: [raster, vector], //raster is a map and vector is the layer to draw
-  target: 'map',
-  view: new View({
-    center: [-8246000, 512500], //Centro historico de Bogota
-    zoom: 16,
-  }),
-});
-
-let draw; // global so we can remove it later
-
-function addInteraction() {
-  const chooseType = typeSelect.value;
-  if (chooseType !== 'None'){
-    draw = new Draw({
-      source: source,
-      type: typeSelect.value,
-      style: function (feature) {
-        return styleFunction(feature, chooseType);
-      },
-    });
-    map.addInteraction(draw);
-  }
-}
-
-addInteraction();
+baseMap();
